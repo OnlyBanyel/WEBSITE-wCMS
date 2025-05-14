ba33091f1bb6a2a294275794dbb1a4794fb3a527
Banyel <hz202300645@wmsu.edu.ph>
2025-05-14 20:57:08 +0800

    d

--- CMS-WMSU-Website/classes/pages.class.php
+++ CMS-WMSU-Website/classes/pages.class.php
@@ -1,5 +1,5 @@
 <?php
-require_once "../tools/functions.php";
+require_once __DIR__ . "../../../tools/functions.php";
 require_once __DIR__ . "/db_connection.class.php";
 
 class Pages {
--- page/admissions/onlineReg.php
+++ page/admissions/onlineReg.php
@@ -1,6 +1,5 @@
 <?php 
 require_once '../../CMS-WMSU-Website/classes/pages.class.php';
-require_once '../../CMS-WMSU-Website/tools/functions.php';
 
 $onlineRegObj = new Pages;
 
