7a2732b787c0c9743483f2a8e37d7f261d16bb70
Banyel <hz202300645@wmsu.edu.ph>
2025-05-14 20:46:53 +0800

    Update pages.class.php

--- CMS-WMSU-Website/classes/pages.class.php
+++ CMS-WMSU-Website/classes/pages.class.php
@@ -1,5 +1,5 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . "/WEBSITE-WCMS/CMS-WMSU-Website/tools/functions.php";
+require_once "../tools/functions.php";
 require_once __DIR__ . "/db_connection.class.php";
 
 class Pages {
