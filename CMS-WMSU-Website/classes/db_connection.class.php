583873b68ac711671738e103762744932fb88935
Banyel <hz202300645@wmsu.edu.ph>
2025-05-14 10:58:53 +0800

    this

--- CMS-WMSU-Website/classes/db_connection.class.php
+++ CMS-WMSU-Website/classes/db_connection.class.php
@@ -1,24 +1,23 @@
 <?php
 
-    class Database{
-        private $dbhost = "localhost";
-        private $dbname = "wmsuCMS";
-        private $user = "root";
-        private $password = "";
+class Database {
+    private $dbhost = getenv('DB_HOST');  // Use environment variable for host
+    private $dbname = getenv('DB_NAME');  // 'wmsucms'
+    private $user = getenv('DB_USER');   // 'root'
+    private $password = getenv('DB_PASS'); // Empty password
 
-        protected $db;
+    protected $db;
 
-        function connect(){
-            
-            try{
+    function connect(){
+        try {
             $this->db = new PDO("mysql:host=$this->dbhost;dbname=$this->dbname", $this->user, $this->password);
-        }               
-            catch(PDOException $e){
-            echo "connection error" . $e->getMessage();
+            // Set the PDO error mode to exception
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $e) {
+            echo "Connection error: " . $e->getMessage();
         }
 
         return $this->db;
     }
-
 }
 ?>
