424fe506490af8cd67d749215c59faf34ac798e0
Banyel <hz202300645@wmsu.edu.ph>
2025-05-16 02:20:52 +0800

    Update db_connection.class.php

--- CMS-WMSU-Website/classes/db_connection.class.php
+++ CMS-WMSU-Website/classes/db_connection.class.php
@@ -9,24 +9,27 @@ class Database {
     protected $db;
 
     public function __construct() {
-    $requiredVars = ['DB_HOST', 'DB_PORT', 'DB_NAME', 'DB_USER', 'DB_PASS'];
-    foreach ($requiredVars as $var) {
-        if (empty(getenv($var))) {
-            throw new Exception("Missing required environment variable: $var");
-        }
-    }
+    // $requiredVars = ['DB_HOST', 'DB_PORT', 'DB_NAME', 'DB_USER', 'DB_PASS'];
+    // foreach ($requiredVars as $var) {
+    //     if (empty(getenv($var))) {
+    //         throw new Exception("Missing required environment variable: $var");
+    //     }
+    // }
 
-    $this->dbhost = getenv('DB_HOST');
-    $this->port = getenv('DB_PORT');
-    $this->dbname = getenv('DB_NAME');
-    $this->user = getenv('DB_USER');
-    $this->password = getenv('DB_PASS');
+    $this->dbhost = 'wmsucms-wmsu-act.h.aivencloud.com';
+    $this->port = '20747';
+    $this->dbname = 'wmsucms';
+    $this->user = 'avnadmin';
+    $this->password = 'AVNS_suifDF-uCj0_ikObk7C';
+    
+    // Rest of your connection code
+}
 
-    error_log("DB Connection Config:");
-    error_log("Host: {$this->dbhost}");
-    error_log("Port: {$this->port}");
-    error_log("User: {$this->user}");
-    error_log("DB Name: {$this->dbname}");
+    // error_log("DB Connection Config:");
+    // error_log("Host: {$this->dbhost}");
+    // error_log("Port: {$this->port}");
+    // error_log("User: {$this->user}");
+    // error_log("DB Name: {$this->dbname}");
 
         // $this->dbhost   = getenv('DB_HOST') ?: '127.0.0.1';
         // $this->dbname   = getenv('DB_NAME') ?: 'wmsucms';
